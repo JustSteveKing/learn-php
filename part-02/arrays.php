332677b722d2030393aa04fba5e03ccd18f44aec
Steve McDougall <juststevemcd@gmail.com>
2021-02-01 10:00:04 +0000

    Standard arrays

--- part-02/arrays.php
+++ part-02/arrays.php
@@ -9,3 +9,17 @@ var_dump($empty);
 $lotteryNumbers = [
     04, 28, 31, 36, 51, 55, 42
 ];
+
+$names = [
+    'Steve',
+    'Dan',
+    'Amy',
+    'Faye',
+];
+
+// With arrays we can perform actions on each item.
+
+// lets take our names array, and for each one of them -> do something with the current name we are on.
+foreach ($names as $name) {
+    echo $name;
+}
